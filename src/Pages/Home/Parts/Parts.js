77e31148753ff1c2ee3e87a448959cde6f44fd85
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-05-23 01:51:27 +0600

    added all property in a card

--- src/Pages/Home/Parts/Parts.js
+++ src/Pages/Home/Parts/Parts.js
@@ -10,26 +10,39 @@ const Parts = () => {
         Our Awesome <span className="text-primary">Services</span>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mx-auto px-6 md:px-10 lg:px-20">
-        {parts.slice(0, 4).map(({ _id, img, title, description, price }) => (
-          <div className="card w-full bg-base-100 px-4 shadow-lg" key={_id}>
-            <figure>
-              <img src={img} alt="" className="rounded-xl w-100" />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{title}</h2>
-              <p className="card-text text-primary font-bold">${price}</p>
-              <p className="card-text">
-                {description?.slice(0, 80)}
-                {description?.length > 100 && (
-                  <span title={`${description}`}>...read more</span>
-                )}
-              </p>
-              <button className="btn btn-primary flex mx-auto mt-4 text-white rounded px-6">
-                Buy Now
-              </button>
-            </div>
-          </div>
-        ))}
+        {parts
+          .slice(0, 4)
+          .map(
+            ({ _id, img, title, description, price, minimum, available }) => (
+              <div className="card w-full bg-base-100 px-4 shadow-lg" key={_id}>
+                <figure>
+                  <img src={img} alt="" className="rounded-xl w-100" />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <h2 className="card-title">{title}</h2>
+                  <p className="card-text font-bold">
+                    Price: <span className="text-primary">${price}</span>
+                  </p>
+                  <p className="card-text font-bold">
+                    Minimum: <span className="text-primary">${minimum}</span>
+                  </p>
+                  <p className="card-text font-bold">
+                    Available:{" "}
+                    <span className="text-primary">${available}</span>
+                  </p>
+                  <p className="card-text">
+                    {description?.slice(0, 80)}
+                    {description?.length > 100 && (
+                      <span title={`${description}`}>...read more</span>
+                    )}
+                  </p>
+                  <button className="btn btn-primary flex mx-auto mt-4 text-white rounded px-6">
+                    Buy Now
+                  </button>
+                </div>
+              </div>
+            )
+          )}
       </div>
       <Link to="/all-services">
         <button className="btn btn-primary flex mx-auto mt-16 text-white rounded px-10">
