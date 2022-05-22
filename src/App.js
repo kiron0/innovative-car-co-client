29cccd18c0cdea8bb9daac339c54a71af44a648d
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-05-22 21:30:30 +0600

    login implement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./Pages/Home/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import SignUp from "./Pages/Login/SignUp/SignUp";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>Innovative Car Co</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
